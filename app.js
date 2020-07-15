bf18979536f3779179f80571791fee17f450e828
batkhongor <bathongor142@gmail.com>
2020-07-15 18:47:03 +0800

    shog shideg bolgoh

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ var roundScore = 0;
 
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
 
-var dice = Math.floor(Math.random() * 6) + 1;
-
-/* <div class="player-score" id="score-0">43</div> */
-// window.document.querySelector("#score-0").textContent = dice;
-
-// window.document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
-
 // Програм эхлэд билдэх
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
 
-document.querySelector(".dice").style.display = "none";
-console.log("Шоо :" + dice);
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
