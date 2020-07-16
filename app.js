c39aa915f21c6721008d3aa6e9bb527d3af0ed2a
batkhongor <bathongor142@gmail.com>
2020-07-16 14:02:30 +0800

    toglogchiin eeljiig solidog bolgon

--- app.js
+++ app.js
@@ -20,9 +20,34 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх эвэнт листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 дотор санамсаргүй тоо гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+  // Шоо зургийг вэб дээр гаргаж ирнэ
   diceDom.style.display = "block";
+  // буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ
   diceDom.src = "dice-" + diceNumber + ".png";
+  //буусан тоон нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийг оноог нэмэгдүүлнэ.
+  if (diceNumber !== 1) {
+    //   1 ээс Ялгаатай тоо буулгаа. Буусан тоог тоглогчид нэмж өгнө
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //   1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+    //     энэ тоглогчийн ээлжиндээ цуглуулсан оноог нойлно
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    // хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго
+    // үгүй бол идвэхтэй тоглогчийг 0 болго.
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    //улаан цэгийг шилжүүлэх
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //шоог түр алга болгон
+    diceDom.style.display = "none";
+  }
 });
